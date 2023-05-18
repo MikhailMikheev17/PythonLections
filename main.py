9b47c30285cd6ef7ccd718ce84d08cc8849a1d82
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-05-18 16:44:01 +0300

    ended main

--- main.py
+++ main.py
@@ -17,9 +17,66 @@
 # print(c )
 # print(type(c))
 
-print('Enter first number:')
-a = int(input())
+# print('Enter first number:')
+# a = int(input())
 
-b = int(input('Enter the secod number: '))
+# b = int(input('Enter the secod number: '))
 
-print(a,'+',b,'=',a+b)
+# print(a,'+',b,'=',a+b)
+
+# a =5.3468
+# b = 6.39053905
+# print(round((a*b),3))
+
+# username = input('Enter your name:')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Марина':
+#     print('qwer')
+# elif username == 'Ильнар':
+#     print('qw')
+# else:
+#     print('Hi,', username)
+
+# i = 0
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('хватит ) ')    
+# print(i)    
+
+# n =int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:#если остаток при делении числа n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n // 2:# делитель числа не может превышать введенное число, деленное на 2 
+#         print(n)
+#         flag = False
+#     i += 1        
+
+# a = 'qwerty'
+# # print(a[0])
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*"
+#     print(line)    
+
+text = 'СъЕШЬ еще этих МяГкИх французских булок'
+# print(len(text)) # позволяет узнать размер
+# print('еще' in text) # поиск элемента 
+# print(text.lower()) # перевод в нижний регистр 
+# print (text.upper())# в верхний регистр 
+# print (text.replace('еще',' tot'))# замена элемента 
+
+#print(text[len(text)-1]) == print(text[-1])
