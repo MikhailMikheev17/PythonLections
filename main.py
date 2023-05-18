dd0d3a3c93e79cda916ab407387b6aa4f0032233
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-05-18 16:05:39 +0300

    add main

---
+++ main.py
@@ -0,0 +1,25 @@
+# a = 5
+# b = 5.9
+# c = 'hello'
+# print("{}-{}-{}".format(a,b,c))
+# print(f"{a}-{b}-{c}")
+# print('Enter first number:')
+# a = input()
+
+# b = input('Enter the secod number: ')
+
+# print(a,'+',b,'=',a+b)
+#Приведение к типу данных
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c )
+# print(type(c))
+
+print('Enter first number:')
+a = int(input())
+
+b = int(input('Enter the secod number: '))
+
+print(a,'+',b,'=',a+b)
